405c21649ecbb408342091238297755800569319
Temp <temp@admins-MacBook-Air-2.local>
2025-03-10 17:44:39 +0530

    header ans section started

--- src/sections/header/Header.tsx
+++ src/sections/header/Header.tsx
@@ -1,6 +1,20 @@
 import "./Header.css";
+
 export default function Header() {
+
+
+
     return (
-        <div className="h">Header</div>
-    )
+        <header className="headerCon">
+            <div className="logo">Barro.</div>
+            <div className="menu iBlock">
+                [=]
+            </div>
+            <div className="items">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Services</a>
+            </div>
+        </header>
+    );
 }
