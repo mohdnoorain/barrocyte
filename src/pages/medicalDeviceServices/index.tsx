3564eb6f790c62a98c025914ec58a4e23c638403
Dheeru12503 <kumardheeraj12503@gmail.com>
2025-04-01 17:16:30 +0530

    Update Footer Section & Links on Regulatory Solution

--- src/pages/medicalDeviceServices/index.tsx
+++ src/pages/medicalDeviceServices/index.tsx
@@ -3,16 +3,15 @@ import "@/styles/globals.css";
 import "./index.css";
 import MedicalDevicesRegulatoryServices from "@/sections/medicalDevicesRegulatoryServices/MedicalDevicesRegulatoryServices";
 import MedicalDevicesManufacturingServices from "@/sections/medicalDevicesManufacturingServices/MedicalDevicesManufacturingServices";
-
-
+import Footer from "@/sections/footer/Footer";
 
 const MedicalDevicesServices = () => {
   return (
     <div>
       <MedicalDevicesRegulatoryServices />
       <hr className="Divider" />
-  
       <MedicalDevicesManufacturingServices />
+      <Footer />
     </div>
   );
 };
