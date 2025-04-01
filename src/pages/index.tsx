f7d1e8976506a387fe60873a07b63c2a24cc1a60
Dheeru12503 <142683911+Dheeru12503@users.noreply.github.com>
2025-04-02 03:30:00 +0530

    Revert "Diagnostic Kits Services Section"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,15 +2,11 @@ import Head from "next/head";
 import "./styles.css";
 import "./../styles/globals.css";
 
+
 import Main from "@/sections/main/Main";
 import SanitaryImportPermitService from "@/sections/sanitaryImportPermitService/SanitaryImportPermitService";
 import RegulatorySolutions from "@/sections/regulatorySolutions/RegulatorySolutions";
 import Header from "@/components/header/Header";
-import BloodBankServive from "@/sections/bloodBankService/BloodBankServive";
-import DrugsLicenseServices from "@/sections/drugs/DrugsLicenseServices";
-import Footer from "@/sections/footer/Footer";
-
-
 
 export default function Home() {
   return (
@@ -25,13 +21,10 @@ export default function Home() {
           href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
         />
       </Head>
-      <Header />
+      <Header/>
       <Main></Main>
       <RegulatorySolutions />
-      <DrugsLicenseServices/>
-      <BloodBankServive />
       <SanitaryImportPermitService />
-      <Footer/>
     </>
   );
 }
