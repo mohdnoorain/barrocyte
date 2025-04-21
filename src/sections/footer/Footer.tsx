36461e3754b33d2540ab96755bf5e3fda587631c
Dheeru12503 <kumardheeraj12503@gmail.com>
2025-04-22 03:35:02 +0530

    Added map in footer section & change contact information

--- src/sections/footer/Footer.tsx
+++ src/sections/footer/Footer.tsx
@@ -46,10 +46,34 @@ const Footer = () => {
           <div className={styles.footerSection}>
             <h3>Contact Us</h3>
             <p>
-              Email: <a href="mailto:info@barr.com">info@barr.com</a>
+              Email:
+              <a href="mailto:info@barr.com">
+                barrocyte@gmail.com / info@barrocyte.com
+              </a>
             </p>
             <p>Phone: +91-9266665237</p>
-            <p>Address: 123 Business Street, City, India</p>
+            <p>
+              Address: Office No.105, First Floor,
+              <br /> CSC Market, Sukhdev Vihar,
+              <br />
+              New Delhi- 110025
+            </p>
+          </div>
+          {/* Map Location */}
+          <div className={styles.footerSection}>
+            <h3>Our Location</h3>
+            <div className={styles.mapContainer}>
+              <iframe
+                title="Office Location"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d14017.003255865014!2d77.2660722!3d28.5624284!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390ce3a33dbddf13%3A0x30cfbbc385740c4c!2sSukhdev%20Vihar%2C%20New%20Delhi%2C%20Delhi%20110025!5e0!3m2!1sen!2sin!4v1714043235003!5m2!1sen!2sin"
+                width="100%"
+                height="200"
+                style={{ border: 0 }}
+                allowFullScreen
+                loading="lazy"
+                referrerPolicy="no-referrer-when-downgrade"
+              ></iframe>
+            </div>
           </div>
         </div>
 
